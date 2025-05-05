7a07659725127658e64f1ecdada7dc251ccdb71d
Rathod Divyaraj <divayrajrathod69@gmail.com>
2025-05-05 16:03:29 +0530

    final ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="https://github.com/rathoddivy/Protfolio">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
